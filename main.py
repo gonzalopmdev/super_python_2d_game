8fe09eaafedec3c19b1953ebf7d35614824d4805
Gonzalo Palma Menéndez <gonzalopm3d@gmail.com>
2024-06-22 19:19:33 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+from settings import *
+import pygame
+import sys
+from os.path import join
+from os.path import dirname, realpath, exists
+from level import Level
+from pytmx.util_pygame import load_pygame
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Super Pirate World')
+        
+        
+        # get actual route from main.py
+        base_path = os.path.dirname(__file__)
+        
+        #Build main route to tmx file
+        script_dir = dirname(realpath(__file__))
+        self.tmx_path = join(script_dir, 'data', 'levels', 'omni.tmx')
+
+       
+        #Load tmx file
+        self.tmx_maps = {0: load_pygame(self.tmx_path)}
+        
+        
+        self.current_stage = Level(self.tmx_maps[0])
+    
+        
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+            self.current_stage.run()        
+                    
+            pygame.display.update()
+            
+if __name__ == '__main__':
+	game = Game()
+	game.run()
